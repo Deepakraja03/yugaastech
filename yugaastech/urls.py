03434373900e64899d1df779c996548c7827c361
Deepakraja03 <12306deepakraja@gmail.com>
2023-11-02 19:21:20 +0530

    complete

--- yugaastech/urls.py
+++ yugaastech/urls.py
@@ -1,6 +1,6 @@
 """yugaastech URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
@@ -32,7 +32,7 @@ urlpatterns = [
 ]
 """yugaastech URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
